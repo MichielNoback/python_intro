ce66e29ae6f63e21f56a4b36e8b3d23bd0b8c126
Michiel <m.a.noback@pl.hanze.nl>
2023-03-14 11:48:08 +0100

    OO exercise

--- scripts/triangle_surface.py
+++ scripts/triangle_surface.py
@@ -4,9 +4,10 @@ triangles with sides given on the command line.'''
 import sys
 
 def print_triangle_area(side):
-    area =  0.5 * side**2
-    print(f'The area of an equilateral triangle with side \
-        {side} is {round(area, 2)}')
+    '''calculates the surface of a triangle'''
+    area =  round(0.5 * side**2, 2)
+    # now print the stuff
+    print(f'The area of an equilateral triangle with side {side} is {area}')
 
 for arg in sys.argv[1:]:
     side = float(arg)
